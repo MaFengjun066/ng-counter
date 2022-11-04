2e2cecad8b879d54f2e9f26049c5bae52fa201cd
Fengjun Ma <FMa6@slb.com>
2022-11-04 15:40:43 +0800

    feat: add counter-group component

---
+++ src/app/counter-group/counter-group.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-group',
+  templateUrl: './counter-group.component.html',
+  styleUrls: ['./counter-group.component.scss']
+})
+export class CounterGroupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
